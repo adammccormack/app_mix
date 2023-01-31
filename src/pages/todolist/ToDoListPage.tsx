3f31a0a226f423034a1567ba3ab0d1b81d4d04c2
Adam <adam.j.mccor@gmail.com>
2023-01-31 23:38:23 +0000

    removed bulma css and built own navbar and to do list border

--- src/pages/todolist/ToDoListPage.tsx
+++ src/pages/todolist/ToDoListPage.tsx
@@ -1,7 +1,30 @@
+import styles from "../todolist/ToDoList.module.css";
+
 export const ToDoListPage = () => {
   return (
-    <header>
-      <h1>To Do List Page</h1>
-    </header>
+    <>
+      <header>
+        <h1>To Do List</h1>
+      </header>
+      <div className={styles.toDoListContainer}>
+        {/* <h3 className="toDoListHeader">Items</h3> */}
+        <div className={styles.taskForm}>
+          <div className={styles.inputContainer}>
+            <input
+              id="firstname"
+              className={styles.input}
+              type="text"
+              placeholder=" "
+            />
+            <label htmlFor="toDoListItem" className={styles.placeholder}>
+              Write Here
+            </label>
+          </div>
+          <button type="submit" className={styles.submit}>
+            add
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
