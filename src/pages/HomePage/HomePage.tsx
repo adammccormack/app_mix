b6bb8331f584a9dcd634852a604f44d6d9c08462
Adam <adam.j.mccor@gmail.com>
2023-02-02 08:44:30 +0000

    removed unnecessary html

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,8 +2,6 @@ export const HomePage = () => {
   return (
     <div className="container">
       <h1>App Mix</h1>
-      <h2>Hi my name is</h2>
-      <h2>What my name is</h2>
     </div>
   );
 };
